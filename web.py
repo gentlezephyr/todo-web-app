965dffe8eaef189e26f95082fdfb73f44adda7c2
Elara <171523581+gentlezephyr@users.noreply.github.com>
2024-08-19 22:41:50 -0300

    Teste

--- web.py
+++ web.py
@@ -16,11 +16,11 @@ st.write('This app is to increase your productivity')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=f'checkbox_{index}')
-    if checkbox:
-        todos.pop(index)
-        functions.write_todos(todos)
-        del st.session_state[f'checkbox_{index}']
-        st.rerun()
+    # if checkbox:
+    #     todos.pop(index)
+    #     functions.write_todos(todos)
+    #     del st.session_state[f'checkbox_{index}']
+    #     st.rerun()
 
 st.text_input(label='Enter a todo', placeholder='Enter a todo...',
               label_visibility="hidden",
